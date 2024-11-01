aa4ca8852a9edd12532339863ea87541e9c4f145
Hanbo <litterpigger@gmail.com>
2024-11-01 12:42:04 +0800

    plot

--- python_tester/__main__.py
+++ python_tester/__main__.py
@@ -32,36 +32,25 @@ def main():
         sys.exit(1)
     
     period=int(strategy_param[1])
-    print(df.head())
-    print(df.index.name)
-
     data=Backtester(df).get_data_pd()
-    cerebro=bt.Cerebro()
+    cerebro=bt.Cerebro(stdstats=False)
+    cerebro.addobserver(bt.observers.Value) 
     cerebro.adddata(data)
-    cerebro.run()
-     
-    cerebro=bt.Cerebro()
-    cerebro.adddata(data)
-  
     cerebro.addstrategy(TurtleStrategy,period_high=period)
     cerebro.addanalyzer(bt.analyzers.SharpeRatio, _name='sharpe')
     cerebro.addanalyzer(bt.analyzers.DrawDown, _name='drawdown')
     cerebro.addanalyzer(bt.analyzers.Returns, _name='returns')
-    cerebro.broker.setcash(10_000_000)  # 设置初始资金为1000万
+    cerebro.broker.setcash(10_000_000) 
     cerebro.broker.setcommission(leverage=20.0)
     cerebro.broker.set_slippage_fixed(1)
     print('初始资金: %.2f' % cerebro.broker.getvalue())
     results = cerebro.run()
-    strat = results[0]
-    
-    # 输出分析结果
+    strat = results[0] 
+   
     print('最终资金: %.2f' % cerebro.broker.getvalue())
     print('夏普比率:', strat.analyzers.sharpe.get_analysis()['sharperatio'])
     print('最大回撤: %.2f%%' % strat.analyzers.drawdown.get_analysis()['max']['drawdown'])
     print('年化收益: %.2f%%' % (strat.analyzers.returns.get_analysis()['rnorm100']))
-    
-    # 绘制结果
     cerebro.plot(style='candle', volume=False)
-    cerebro.plot(style='bar')
     
 main()
